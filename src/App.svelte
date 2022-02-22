ba5d9d40531070443cf85906dcc2c3f8bb03d542
AxelZublena <zubl0001@hz.nl>
2022-02-22 22:26:26 +0100

    Simple client

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import DownloadButton from "./components/DownloadButton.svelte";
+
+	let url = "/";
+	let fileName = "testPdf.pdf";
+
+	async function handleImageUpload(event) {
+		const file = event.target.files[0];
+		const formData = new FormData();
+		formData.append("testFile", file);
+
+		const response = await fetch("http://localhost:8080/", {
+			method: "POST",
+			body: formData,
+			mode: "cors",
+		});
+		const body = await response.blob();
+		const blob = new Blob([body], { type: "application/pdf" });
+
+		// const textStream = body.pipeThrough(new ReadableStream());
+		// const outFile = new File([body], { type: "application/pdf" });
+
+		url = URL.createObjectURL(blob);
+	}
+</script>
+
+<main>
+	<h1>Hello!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+	<input type="file" id="fileUpload" on:change={handleImageUpload} />
+	<a href={url} target="_blank">URL OF FILE</a>
+	<!-- <DownloadButton fileURL={url} {fileName} text="Download PDF" /> -->
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
---
+++ src/components/DownloadButton.svelte
@@ -0,0 +1,26 @@
+<script>
+	export let fileURL;
+	export let fileName;
+	export let text;
+</script>
+
+<div>
+	<a href={fileURL} download={fileName}
+		><button
+			class="bg-blue-400 hover:bg-gray-400 w-full sm:w-auto justify-center text-gray-900 font-bold px-4 py-3 rounded-xl inline-flex items-center"
+			><svg
+				class="h-8 w-8"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+				/></svg
+			>
+			<span class="pl-2">{text}</span></button
+		></a
+	>
+</div>
